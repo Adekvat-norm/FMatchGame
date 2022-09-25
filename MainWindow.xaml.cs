0d6383247705a936b7c6fe6028c3764634846fd8
Денис Талышкин <denis.talyshkin@mail.ru>
2022-09-25 12:52:46 +0300

    Доделан таймер

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,17 +15,31 @@ using System.Windows.Shapes;
 
 namespace FMatchGame
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    using System.Windows.Threading;
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int matchesFound; // сравнений найдено
+        int TenthsOfSecondsElapsed; // отслеживание прошедшего времени
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
         }
 
+        private void Timer_Tick(object sender, EventArgs e) // должно вызываться при нажатии таб после += строки(29)
+        {
+            TenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (TenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Сыграть снова? ";
+            }
+        }
         private void SetUpGame()
         {
             List<string> animalMemes = new List<string>()
@@ -42,10 +56,48 @@ namespace FMatchGame
             Random random = new Random();
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                int index = random.Next(animalMemes.Count);
-                string NextMeme = animalMemes[index];
-                textBlock.Text = NextMeme;
-                animalMemes.RemoveAt(index);
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalMemes.Count);
+                    string NextMeme = animalMemes[index];
+                    textBlock.Text = NextMeme;
+                    animalMemes.RemoveAt(index);
+                } 
+            }
+            timer.Start();
+            TenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (lastTextBlockClicked.Text == textBlock.Text)
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }    
+        }
+
+        private void timeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame(); // Чтобы сбросить игру, если были найдены 8 пар.
             }
         }
     }
